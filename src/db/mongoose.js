bfaba94e10d9bfeaba13a0adceac67459e5875a0
AIbrayev <aibrayev@one.kz>
2021-03-26 11:31:34 +0600

    feat: validation and sanitization for user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -8,15 +9,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be positive number.')
+      }
+    }
   },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+    trim: true,
+    validate(value) {
+      if (value === 'password') {
+        throw new Error('Your password should not contain "password"')
+      }
+    }
+  }
 });
 
 const me = new User({
-  name: 'Aidyn',
+  name: '  Aidyn  ',
   age: 21,
+  password: 'password'
 });
 
 me.save()
