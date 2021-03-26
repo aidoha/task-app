1601b11a343e07b03caf341534962a0f3ccbe045
AIbrayev <aibrayev@one.kz>
2021-03-26 11:36:14 +0600

    ready for task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,47 +6,70 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true,
 });
 
-const User = mongoose.model('User', {
-  name: {
+// const User = mongoose.model('User', {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error('Email is invalid')
+//       }
+//     }
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error('Age must be positive number.')
+//       }
+//     }
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minLength: 6,
+//     trim: true,
+//     validate(value) {
+//       if (value === 'password') {
+//         throw new Error('Your password should not contain "password"')
+//       }
+//     }
+//   }
+// });
+
+// const me = new User({
+//   name: '  Aidyn  ',
+//   age: 21,
+//   password: 'password'
+// });
+
+// me.save()
+//   .then(() => console.log(me))
+//   .catch((err) => console.log(err));
+
+
+const Task = mongoose.model('Task', {
+  description: {
     type: String,
     required: true,
     trim: true
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid')
-      }
-    }
+  completed: {
+    type: Boolean,
+    default: false
   },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be positive number.')
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-    trim: true,
-    validate(value) {
-      if (value === 'password') {
-        throw new Error('Your password should not contain "password"')
-      }
-    }
-  }
 });
 
-const me = new User({
-  name: '  Aidyn  ',
+const me = new Task({
+  description: '   ',
   age: 21,
   password: 'password'
 });
